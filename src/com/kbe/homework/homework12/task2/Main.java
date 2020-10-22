d1bd4c0bd800028057047bef792699b001ccaf88
Boris <krinitsky.B@yandex.ru>
2020-10-22 23:25:03 +0300

    Fix mistakes

--- src/com/kbe/homework/homework12/task2/Main.java
+++ src/com/kbe/homework/homework12/task2/Main.java
@@ -15,34 +15,41 @@ public class Main {
             arr[7] = 12;
         } catch (Exception e) {
             exepArr[0] = e;
-            try {
-                String str = null;
-                str.trim();
-            }catch (Exception e1){
-                exepArr[1] = e1;
-                try {
-                    int number = 1 / 0;
-                }catch (Exception e2){
-                    exepArr[2] = e2;
-                    try {
-                        Object str = "s";
-                        int number = (Integer) str;
-                    }catch (Exception e3){
-                        exepArr[3] = e3;
-                        try {
-                            int[] arr = new int[-1];
-                        }catch (Exception e4){
-                            exepArr[4] = e4;
-                            try {
-                                char ch = "one".charAt(5);
-                            }catch (Exception e5){
-                                exepArr[5] = e5;
-                            }
-                        }
-                    }
-                }
-            }
         }
+
+        try {
+            String str = null;
+            str.trim();
+        } catch (Exception e1) {
+            exepArr[1] = e1;
+        }
+
+        try {
+            int number = 1 / 0;
+        } catch (Exception e2) {
+            exepArr[2] = e2;
+        }
+
+        try {
+            Object str = "s";
+            int number = (Integer) str;
+        } catch (Exception e3) {
+            exepArr[3] = e3;
+        }
+
+        try {
+            int[] arr = new int[-1];
+        } catch (Exception e4) {
+            exepArr[4] = e4;
+        }
+
+        try {
+            char ch = "one".charAt(5);
+        } catch (Exception e5) {
+            exepArr[5] = e5;
+        }
+
+
         for (int i = 0; i < 6; i++) {
             System.out.println(exepArr[i].toString());
         }
